68084e1cb2b6271fe76380057d1dc6cfa03ebae4
Simon Pettersson <simon.v.pettersson@gmail.com>
2018-08-06 19:59:41 +0200

    Make a proper test for websockets

--- test/services/websocket/test.cpp
+++ test/services/websocket/test.cpp
@@ -1,5 +1,5 @@
-//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-//#include <doctest.h>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
 #include <rxcpp/rx.hpp>
 #include <trawler/logging/logger.hpp>
 #include <trawler/services/service-context.hpp>
@@ -10,75 +10,40 @@
 //#include <websocket-ssl-client.hpp>
 
 using namespace trawler;
-using namespace std::chrono_literals;
 
-void
-rethrow_exception(const std::exception_ptr& eptr)
-{
-  try {
-    if (eptr) {
-      std::rethrow_exception(eptr);
-    }
-  } catch (const std::exception& e) {
-    std::cout << "Caught exception \"" << e.what( ) << "\"\n";
-  }
-}
-
-int
-main( )
-{
-  using namespace std::chrono_literals;
-
+SCENARIO("Websocket server and client") {
   Logger::set_log_level(Logger::ELogLevel::DEBUG);
 
   {
     auto context = make_service_context(4, 4);
-    {
-      auto srv = create_websocket_server(context, "0.0.0.0", 5000);
 
-      auto on_next = [](ServicePacket data) { std::cout << "DATA: " << data.get_payload( ) << '\n'; };
+    auto filter_data = [](auto packet) { return packet.get_status( ) == ServicePacket::EStatus::DATA_TRANSMISSION; };
 
-      auto on_complete = []( ) { std::cout << "COMPLETED\n"; };
+    auto send_reply = [](const std::string& rep) {
+      return [rep](ServicePacket packet) { packet.call_on_reply(std::move(rep)); };
+    };
 
-      auto filter_data = [](auto packet) { return packet.get_status( ) == ServicePacket::EStatus::DATA_TRANSMISSION; };
+    create_websocket_server(context, "0.0.0.0", 5000)
+      .observe_on(rxcpp::observe_on_new_thread( ))
+      .filter(filter_data)
+      .subscribe(send_reply("PONG"));
 
-      auto send_reply = [](const std::string& rep) {
-        return [rep](ServicePacket packet) {
-		 std::cout << "Replying with " << rep << '\n';
-		 packet.call_on_reply(std::move(rep));
-	       };
-      };
+    std::vector<std::string> result{};
 
-      auto srvsub = srv.observe_on(rxcpp::observe_on_new_thread( ))
-                      .filter(filter_data)
-                      .subscribe(send_reply("PONG"), rethrow_exception, on_complete);
-
-      /*
-      auto cli2 = create_websocket_client(context, "localhost", 5000, { "client-2" })
-                    .tap(send_reply("PING"))
-                    .filter(filter_data);
-
-      int i = 0;
-      auto thread = rxcpp::observe_on_event_loop( );
-      rxcpp::observable<>::from(cli1.as_dynamic( ), cli2)
-                      .merge()
-                      .take(10)
-                      .as_blocking( )
-                      .subscribe([&](auto s) { std::cout << "Recv " << ++i << ": " << s.get_payload( ) << '\n'; });
-      */
-
-      // .take(10) causes create_websocket_client_impl to leak!
-      int i = 0;
-
-      create_websocket_client(context, "localhost", 5000, { "client-1" })
-        .observe_on(rxcpp::observe_on_new_thread( ))
-        .tap(send_reply("PING"))
-        .filter(filter_data)
-        .take(10)
-        .as_blocking( )
-        .subscribe([&](auto s) { std::cout << "Recv " << ++i << ": " << s.get_payload( ) << '\n'; });
-
-    }
+    create_websocket_client(context, "localhost", 5000, { "client-1" })
+      .observe_on(rxcpp::observe_on_new_thread( ))
+      .tap(send_reply("PING"))
+      .filter(filter_data)
+      .take(10)
+      .reduce(std::vector<std::string>{},
+              [](auto acc, auto s) {
+		CHECK( s.get_payload() == "PONG" );
+                acc.push_back(s.get_payload( ));
+                return acc;
+              })
+      .as_blocking( )
+      .subscribe([&](auto r) { result = r; });
+    
+    CHECK(result.size() == 10);
   }
-  return 0;
 }

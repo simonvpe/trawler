0c405866e66af95fad46ffbcee0b4ea8d60b2748
Simon Pettersson <simon.v.pettersson@gmail.com>
2018-08-20 21:07:50 +0000

    Closer to a working http-server

--- test/services/http-server/test.cpp
+++ test/services/http-server/test.cpp
@@ -5,14 +5,58 @@
 #include <boost/asio/bind_executor.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <nlohmann/json.hpp>
 #include <trawler/services/service-packet.hpp>
 #include <trawler/services/tcp-common/make-tcp-acceptor.hpp>
 #include <trawler/services/tcp-common/make-tcp-listener.hpp>
 namespace trawler {
 
+template<typename DoRead>
+void
+run_http_event_loop(DoRead do_read)
+{
+  namespace http = boost::beast::http;
+  using json = nlohmann::json;
+
+  do_read([=](auto ec, auto logger, auto request, auto buffer, auto on_next, auto on_error, auto on_completed) {
+    using status_t = ServicePacket::EStatus;
+
+    if (ec == http::error::end_of_stream || ec == boost::system::errc::operation_canceled ||
+        ec == boost::asio::error::eof) {
+      logger.info("Connection closed");
+      on_next(status_t::DISCONNECTED);
+      on_completed( );
+      return;
+    }
+
+    if (ec) {
+      logger.info("Error: " + ec.message( ));
+      on_error(make_runtime_error(ec));
+      return;
+    }
+
+    auto json_object = json{ { "method", request->method_string( ) },
+                             { "target", request->target( ) },
+                             { "body", request->body( ) } };
+
+    for (const auto& header : request->base( )) {
+      json_object["headers"][std::string{ header.name_string( ) }] = std::string{ header.value( ) };
+    }
+
+    const auto json_string = json_object.dump( );
+    logger.debug(json_string);
+    on_next(status_t::DATA_TRANSMISSION, json_string);
+
+    run_http_event_loop(do_read);
+  });
+}
+
 auto
 make_http_event_loop(const std::shared_ptr<ServiceContext>& context, const Logger& logger)
 {
+  namespace http = boost::beast::http;
+
   using socket_t = boost::asio::ip::tcp::socket;
   using socket_tp = std::shared_ptr<socket_t>;
   using strand_t = boost::asio::strand<boost::asio::io_context::executor_type>;
@@ -20,7 +64,41 @@ make_http_event_loop(const std::shared_ptr<ServiceContext>& context, const Logge
 
   return [=](const socket_tp& socket) {
     using result_t = ServicePacket;
-    return rxcpp::observable<>::create<result_t>([](auto subscriber) {});
+
+    auto buffer = std::make_shared<boost::beast::flat_buffer>( );
+    auto request = std::make_shared<http::request<http::string_body>>( );
+    auto session_strand = std::make_shared<strand_t>(context->get_session_context( ).get_executor( ));
+    auto service_strand = std::make_shared<strand_t>(context->get_service_context( ).get_executor( ));
+
+    auto on_subscribe = [=](auto subscriber) {
+      using status_t = ServicePacket::EStatus;
+      using data_t = const std::string&;
+
+      auto on_next = [](status_t status, data_t data = "") {};
+
+      auto on_error = [=](std::exception_ptr e) {
+        auto fn = boost::asio::bind_executor(*service_strand, [=] { subscriber.on_error(e); });
+        fn( );
+      };
+
+      auto on_completed = [=]( ) {
+        auto fn = boost::asio::bind_executor(*service_strand, [=]( ) { subscriber.on_completed( ); });
+        fn( );
+      };
+
+      on_next(status_t::CONNECTED);
+
+      auto do_read = [=](auto on_read_impl) {
+        auto read_callback = [=](error_t ec, std::size_t bytes_transferred) {
+          on_read_impl(ec, logger, request, buffer, on_next, on_error, on_completed);
+        };
+        http::async_read(
+          *socket, *buffer, *request, boost::asio::bind_executor(*session_strand, std::move(read_callback)));
+      };
+      run_http_event_loop(std::move(do_read));
+    };
+
+    return rxcpp::observable<>::create<result_t>(std::move(on_subscribe));
   };
 }
 
@@ -41,6 +119,7 @@ create_http_server(const std::shared_ptr<ServiceContext>& context,
 SCENARIO("dummy http-server")
 {
   using namespace trawler;
+  Logger::set_log_level(Logger::ELogLevel::DEBUG);
   auto context = make_service_context( );
   create_http_server(context, "0.0.0.0", 5001, { "my-http-server" }).as_blocking( ).subscribe([](auto s) {
     std::cout << "PAYLOAD: " << s.get_payload( ) << "\n";
